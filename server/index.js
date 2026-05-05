de9cdb8a5f66f82482acb41be224f4e30f2558de
Zakib <zakib6354@gmail.com>
2026-05-05 06:10:44 +0530

    Added the index.js and mongodb connection to and to start the server

---
+++ server/index.js
@@ -0,0 +1,37 @@
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+// Load the environment variables from the .env file
+dotenv.config();
+
+// Initialize the express app
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+const PORT = process.env.PORT || 5000;
+
+//Connect to MongoDB
+mongoose
+  .connect(process.env.MONGODB_URL, {
+    serverSelectionTimeoutMS: 5000,
+  })
+  .then(() => {
+    console.log("MongoDB connected successfully.");
+  })
+  .catch((err) => {
+    console.error("MongoDB connection error:", err.message);
+  });
+
+app.get("/", (req, res) => {
+  res.send("TOD API is running...");
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
